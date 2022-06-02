5ca93105045449553ffb473d1d0ead967597f3ce
theybie <zoyasarow@gmail.com>
2022-06-01 20:05:43 -0600

    fixed styling

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+//time display
+var dateTime = null;
+var date = null;
+
+var update = function () {
+  date = moment(new Date());
+  dateTime.html(date.format("dddd MMMM Do YYYY, hh:mm"));
+};
+
+$(document).ready(function () {
+  dateTime = $("#dateTime");
+  update();
+  setInterval(update, 1000);
+});
