da276c681f8d4b162aa75ce12429e0ce07133c1b
theybie <zoyasarow@gmail.com>
2022-06-06 22:16:04 -0600

    updated css

--- Assets/script.js
+++ Assets/script.js
@@ -18,66 +18,8 @@ $(document).ready(function () {
   setInterval(update, 1000);
 });
 
-//start of exercise API code
-//functionality for exercise search input
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  var workoutSearch = exerciseInput.value;
-
-  if (workoutSearch) {
-      getExerciseOptions(workoutSearch);
-      exerciseResults.textContent = '';
-      exerciseInput.value = '';
-  } else {
-      alert('Please enter fitness term');
-  }
-};
-
-//functionality for fetching exercise API data 
-var getExerciseOptions = function (workoutSearch) {
-  var apiFetch = 'https://wger.de/api/v2/exerciseinfo/?limit=300&offset=0&/language=2'
- 
-  fetch(apiFetch)
-    .then(function(response) {
-        return response.json();
-    }).then(function(response) {
-        console.log(response)
-        var filteredResults = response.results.filter(exercise => exercise.category.name === workoutSearch && exercise.language.short_name === "en")
-        // var results = filterExercise(response, workoutSearch)
-        console.log(filteredResults)
-        renderExercise(filteredResults)
-    })
-}
-
-// functionality for displaying data from workout API
-var renderExercise = function (exercises) {
-  exerciseResults.innerHTML = ''
-
-  //for loop to print individual data on each card
-  for (var i = 0; i < exercises.length; i++) {
-    //creating exercise card elements
-    var exerciseCard = document.createElement('div');
-    exerciseCard.setAttribute('class', 'exercise-card')
-    exerciseResults.append(exerciseCard)
-    //functionality for displaying exercise name 
-    var name = exercises[i].name;
-    var nameEl = document.createElement('h4');
-    nameEl.setAttribute('class', 'nameStyling')
-    nameEl.textContent = name
-    exerciseCard.append(nameEl)
-    //functionality for displaying 
-    var description = exercises[i].description;
-    var descriptionEl = document.createElement('div')
-    descriptionEl.setAttribute('class', 'descriptionStyling')
-    descriptionEl.innerHTML = '<strong>' + description + '</strong>'
-    exerciseCard.append(descriptionEl)
-  }
-}
-exerciseBtn.onclick = formSubmitHandler;
-
-//start of workout calculator 
 //calculate workout time;24 hour time scale
+
 var start = document.getElementById("startInput");
 var end = document.getElementById("endInput");
 var subResult = document.getElementById("subResult");
@@ -131,3 +73,67 @@ saveBtn.addEventListener("click", function (event) {
     userEndSpan.textContent = end;
   }
 });
+
+//start of exercise API code
+//functionality for exercise search input
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var workoutSearch = exerciseInput.value;
+
+  if (workoutSearch) {
+    getExerciseOptions(workoutSearch);
+    exerciseResults.textContent = "";
+    exerciseInput.value = "";
+  } else {
+    alert("Please enter fitness term");
+  }
+};
+
+//functionality for fetching exercise API data
+var getExerciseOptions = function (workoutSearch) {
+  var apiFetch =
+    "https://wger.de/api/v2/exerciseinfo/?limit=300&offset=0&/language=2";
+
+  fetch(apiFetch)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      var filteredResults = response.results.filter(
+        (exercise) =>
+          exercise.category.name === workoutSearch &&
+          exercise.language.short_name === "en"
+      );
+      // var results = filterExercise(response, workoutSearch)
+      console.log(filteredResults);
+      renderExercise(filteredResults);
+    });
+};
+
+// functionality for displaying data from workout API
+var renderExercise = function (exercises) {
+  exerciseResults.innerHTML = "";
+
+  //for loop to print individual data on each card
+  for (var i = 0; i < exercises.length; i++) {
+    //creating exercise card elements
+    var exerciseCard = document.createElement("div");
+    exerciseCard.setAttribute("class", "exercise-card");
+    exerciseResults.append(exerciseCard);
+    //functionality for displaying exercise name
+    var name = exercises[i].name;
+    var nameEl = document.createElement("h4");
+    nameEl.setAttribute("class", "nameStyling");
+    nameEl.textContent = name;
+    exerciseCard.append(nameEl);
+    //functionality for displaying
+    var description = exercises[i].description;
+    var descriptionEl = document.createElement("div");
+    descriptionEl.setAttribute("class", "descriptionStyling");
+    descriptionEl.innerHTML = description;
+    exerciseCard.append(descriptionEl);
+  }
+};
+exerciseBtn.onclick = formSubmitHandler;
