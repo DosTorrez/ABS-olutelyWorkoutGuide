76a0b26e533afc4e4a56544d1b28847063644227
theybie <zoyasarow@gmail.com>
2022-06-06 19:54:36 -0600

    recent merges

--- Assets/script.js
+++ Assets/script.js
@@ -18,7 +18,6 @@ $(document).ready(function () {
   setInterval(update, 1000);
 });
 
-<<<<<<< HEAD
 //start of exercise API code
 //functionality for exercise search input
 var formSubmitHandler = function (event) {
@@ -77,10 +76,8 @@ var renderExercise = function (exercises) {
 }
 exerciseBtn.onclick = formSubmitHandler;
 
-=======
->>>>>>> f52328752d3ca3c224a571b1e23d6d8bf5b3777e
+//start of workout calculator 
 //calculate workout time;24 hour time scale
-
 var start = document.getElementById("startInput");
 var end = document.getElementById("endInput");
 var subResult = document.getElementById("subResult");
@@ -133,69 +130,3 @@ saveBtn.addEventListener("click", function (event) {
     userEndSpan.textContent = end;
   }
 });
-<<<<<<< HEAD
-=======
-//start of exercise API code
-//functionality for exercise search input
-var formSubmitHandler = function (event) {
-  event.preventDefault();
-
-  var workoutSearch = exerciseInput.value;
-
-  if (workoutSearch) {
-    getExerciseOptions(workoutSearch);
-    exerciseResults.textContent = "";
-    exerciseInput.value = "";
-  } else {
-    alert("Please enter fitness term");
-  }
-};
-
-//functionality for fetching exercise API data
-var getExerciseOptions = function (workoutSearch) {
-  var apiFetch =
-    "https://wger.de/api/v2/exerciseinfo/?limit=300&offset=0&/language=2";
-
-  fetch(apiFetch)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-      var filteredResults = response.results.filter(
-        (exercise) =>
-          exercise.category.name === workoutSearch &&
-          exercise.language.short_name === "en"
-      );
-      // var results = filterExercise(response, workoutSearch)
-      console.log(filteredResults);
-      renderExercise(filteredResults);
-    });
-};
-
-// functionality for displaying data from workout API
-var renderExercise = function (exercises) {
-  exerciseResults.innerHTML = "";
-
-  //for loop to print individual data on each card
-  for (var i = 0; i < exercises.length; i++) {
-    //creating exercise card elements
-    var exerciseCard = document.createElement("div");
-    exerciseCard.setAttribute("class", "exercise-card");
-    exerciseResults.append(exerciseCard);
-    //functionality for displaying exercise name
-    var name = exercises[i].name;
-    var nameEl = document.createElement("h4");
-    nameEl.setAttribute("class", "nameStyling");
-    nameEl.textContent = name;
-    exerciseCard.append(nameEl);
-    //functionality for displaying
-    var description = exercises[i].description;
-    var descriptionEl = document.createElement("div");
-    descriptionEl.setAttribute("class", "descriptionStyling");
-    descriptionEl.textContent = description;
-    exerciseCard.append(descriptionEl);
-  }
-};
-exerciseBtn.onclick = formSubmitHandler;
->>>>>>> f52328752d3ca3c224a571b1e23d6d8bf5b3777e
